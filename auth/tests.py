09437ff04032e23bc8f95c293b523b1ec489db07
Ivan <maayoko@gmail.com>
2022-01-07 17:42:20 +0100

    test(auth): check if /login exists

--- auth/tests.py
+++ auth/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class LoginViewTest(TestCase):
+    def test_url_exists(self):
+        response = self.client.get("/login")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("login/index.html")
+        self.assertInHTML(
+            "<input type=\"submit\" value=\"Login\" class=\"btn float-right login_btn\">", str(response.content))
