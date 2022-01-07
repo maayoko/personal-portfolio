d4a071ce44b350013d736f239d115a2ffcf98d8d
Ivan <maayoko@gmail.com>
2022-01-07 18:34:50 +0100

    test(auth): check if /register exists

--- auth/tests.py
+++ auth/tests.py
@@ -5,8 +5,15 @@ from django.test import TestCase
 
 class LoginViewTest(TestCase):
     def test_url_exists(self):
-        response = self.client.get("/login")
+        response = self.client.get("/auth/login")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed("login/index.html")
         self.assertInHTML(
             "<input type=\"submit\" value=\"Login\" class=\"btn float-right login_btn\">", str(response.content))
+
+
+class RegisterViewTest(TestCase):
+    def test_url_exists(self):
+        response = self.client.get("/auth/register")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("register/index.html")
