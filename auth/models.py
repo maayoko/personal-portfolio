a849df8c480a46c75e2de8ad5b22741199c758bb
Ivan <maayoko@gmail.com>
2022-01-08 09:48:49 +0100

    feat(register): create User model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    username = models.CharField()
+    password = models.CharField()
+    full_name = models.CharField()
+    email = models.EmailField()
+    phone = models.CharField()
