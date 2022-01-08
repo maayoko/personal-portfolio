8ee755be987131d6ac88c21820cecc0f912404db
Ivan <maayoko@gmail.com>
2022-01-08 14:20:45 +0100

    test(auth): add tests for login, register, user model

--- portfolioauth/tests.py
+++ portfolioauth/tests.py
@@ -7,12 +7,29 @@ from .models import User
 
 
 class LoginViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        User.objects.create(
+            username="jd",
+            full_name="John Doe",
+            password="jfdjewdewkjdn",
+            email="john@doe.com",
+            phone="+385 99 585 9138")
+
     def test_url_exists(self):
         response = self.client.get("/auth/login")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed("login/index.html")
         self.assertInHTML(
             "<input type=\"submit\" value=\"Login\" class=\"btn float-right login_btn\">", str(response.content))
+
+    def test_user_should_login(self):
+        user_details = {
+            "username": "jd",
+            "password": "jfdjewdewkjdn"
+        }
+        response = self.client.post("/auth/login", user_details)
+        self.assertEqual(response.status_code, 302)
 
 
 class RegisterViewTest(TestCase):
@@ -45,7 +62,7 @@ class UserModelTest(TestCase):
             phone="+385 99 585 9138")
 
     def test_user_details(self):
-        actual_user = User.objects.get(id=1)
+        actual_user = User.objects.get(id=2)
         actual_user = model_to_dict(actual_user)
         expected_user = {
             "full_name": "John Doe",
@@ -55,3 +72,15 @@ class UserModelTest(TestCase):
             "phone": "+385 99 585 9138"
         }
         self.assertDictContainsSubset(expected_user, actual_user)
+
+    def test_username_should_be_unique(self):
+        try:
+            User.objects.create(
+                username="jd",
+                full_name="John Doe",
+                password="pass",
+                email="john@doe.com",
+                phone="+385 99 585 9138")
+        except:
+            print("Rannnnn")
+            self.assertRaises(RuntimeError)
