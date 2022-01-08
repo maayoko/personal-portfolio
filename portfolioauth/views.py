c4972f687eb2361b4c8c862f3cbdc13dda09d295
Ivan <maayoko@gmail.com>
2022-01-08 16:19:05 +0100

    feat(login): check if user exists and passwords matches prior to logging in

--- portfolioauth/views.py
+++ portfolioauth/views.py
@@ -4,8 +4,13 @@ from django.http.request import HttpRequest
 
 from .login_form import LoginForm
 from .register_form import RegisterForm
+from .models import User
 
 # Create your views here.
+
+
+def check_passwords(password1, password2):
+    return password1 == password2
 
 
 def login(request: HttpRequest):
@@ -13,7 +18,12 @@ def login(request: HttpRequest):
         form = LoginForm(request.POST)
 
         if form.is_valid():
-            return HttpResponseRedirect("/dashboard")
+            user: User = User.objects.get(username=request.POST["username"])
+            password_matches = check_passwords(
+                user.password, request.POST["password"])
+
+            if password_matches:
+                return HttpResponseRedirect("/dashboard")
 
         return render(request, "login/index.html", {"errors": form.errors})
 
